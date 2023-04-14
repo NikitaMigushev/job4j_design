9657d8971c3d90c2137c776888a79b823961730c
Nikita Migushev <migushev@gmail.com>
2023-04-14 20:36:02 +0300

    Утверждения с коллекциями [#504885]

---
+++ src/test/java/assertj/SimpleConvertTest.java
@@ -0,0 +1,50 @@
+package assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkToList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "third", "fourth", "fifth");
+        assertThat(list).hasSize(5)
+                .containsExactly("first", "second", "third", "fourth", "fifth")
+                .doesNotContain("sixth", "seventh");
+    }
+
+    @Test
+    void checkToSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "third", "fourth", "fifth");
+        assertThat(set).hasSize(5)
+                .contains("first", "second", "third", "fourth", "fifth")
+                .containsAnyOf("fourth", "third");
+    }
+
+    @Test
+    void checkToMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "third", "fourth", "fifth");
+        assertThat(map).hasSize(5)
+                .containsValues(1, 2, 3, 4)
+                .containsKeys("first", "second", "fourth");
+    }
+}
