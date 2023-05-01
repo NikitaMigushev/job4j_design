2431fc482baa810f18fe53e5e734cebe663b071a
Nikita Migushev <migushev@gmail.com>
2023-05-01 10:39:29 +0300

    2.1. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -13,7 +13,7 @@ public class EvenNumberFile {
             }
             String[] lines = text.toString().split(System.lineSeparator());
             for (String line : lines) {
-                if (Integer.valueOf(line) % 2 == 0) {
+                if (Integer.parseInt(line) % 2 == 0) {
                     System.out.println(line);
                 }
             }
