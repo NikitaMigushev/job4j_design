93a9db81c0166d7c83f4623c3c2e3adee8ae731f
Nikita Migushev <migushev@gmail.com>
2023-05-13 21:07:23 +0300

    0. Join [#6862]

--- db/teens.sql
+++ db/teens.sql
@@ -15,4 +15,4 @@ insert into teens (name, gender) values
 SELECT t1.name AS name1, t1.gender AS gender1, t2.name AS name2, t2.gender AS gender2
 FROM teens t1
 CROSS JOIN teens t2
-WHERE t1.gender <> t2.gender;
+WHERE t1.gender = 'M' and t1.gender <> t2.gender;
